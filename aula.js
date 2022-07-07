f8284aebcfafbcc9a6acdf8bba4355fe1fd87a0f
Matheusrndev <maxteurod10@gmail.com>
2022-07-07 11:13:32 -0300

    structureJavaScript

---
+++ aula.js
@@ -0,0 +1,32 @@
+/*let idade = 24
+//console.log(idade)
+let altura = 170
+//console.log(altura)
+let nomeCompleto = 'Matheus'
+//console.log(nomeCompleto)
+//console.log(nomeCompleto, altura, idade)
+let estaAprovado = true
+let corSelecionado = null
+//criando objeto
+let pessoa = {
+  nome: 'Matheus',
+  idade: 24,
+  estaAprovado: true
+}
+console.log(pessoa)
+*/
+/*
+//Arrays
+let familia = [26, 25, 'matheus', 42]
+console.log(familia.length)
+console.log(familia[2])
+//Funcionalidade '.' Mostra as funcionalidades
+*/
+//Funções- entra da e saida de dados vamos ter que criar uma função
+let corSite = 'azul'
+function resetaCor(cor, tonalidade) {
+  corSite = cor + ' ' + tonalidade
+}
+console.log(corSite)
+resetaCor('verde', 'claro')
+console.log(corSite)
